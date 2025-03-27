b038486814c9592e07f17b1dc6e47522bf532650
Sasha Hurbanava <sasha.hurbnva@gmail.com>
2025-03-27 11:25:49 +0300

    feat: solve three tasks

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -18,7 +18,7 @@
  *
  */
 function getCurrentFunctionName() {
-  throw new Error('Not implemented');
+  return getCurrentFunctionName.name;
 }
 
 /**
@@ -32,8 +32,12 @@ function getCurrentFunctionName() {
  *   getFunctionBody(hiHello) => "function hiHello() { console.log('hello world'); }"
  *
  */
-function getFunctionBody(/* func */) {
-  throw new Error('Not implemented');
+function getFunctionBody(func) {
+  let result = '';
+  if (typeof func === 'function') {
+    result = func.toString();
+  }
+  return result;
 }
 
 /**
@@ -50,8 +54,14 @@ function getFunctionBody(/* func */) {
  *  ]) => [0, 1, 2]
  *
  */
-function getArgumentsCount(/* funcs */) {
-  throw new Error('Not implemented');
+function getArgumentsCount(funcs) {
+  const result = [];
+
+  Array.from(funcs).forEach((el) => {
+    result.push(el.length);
+  });
+
+  return result;
 }
 
 /**
